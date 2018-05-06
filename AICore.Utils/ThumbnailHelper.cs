d188c7c2d3b80627689c3e79bc4ab569302c7821
sincco <tianofxuan@163.com>
2018-05-06 19:37:30 +0800

    page

--- AICore.Utils/ThumbnailHelper.cs
+++ AICore.Utils/ThumbnailHelper.cs
@@ -68,13 +68,16 @@ namespace AICore.Utils
                         rotate = -90;
                     if (prop.Value[0] == 3)
                         rotate = 180;
-                        break;
+                    break;
                 }
             }
-            if( rotate != 0)
+            switch (rotate)
             {
-                bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                case 90: bmp.RotateFlip(RotateFlipType.Rotate90FlipNone); break;
+                case -90: bmp.RotateFlip(RotateFlipType.Rotate270FlipNone); break;
+                case 180: bmp.RotateFlip(RotateFlipType.Rotate180FlipNone); break;
             }
+
             if (bmp.Width > width)
             {
                 double scale = (double)bmp.Width / width;
@@ -82,8 +85,8 @@ namespace AICore.Utils
 
                 //Image.ThumbMode mode = mode = Image.ThumbMode.Max;
                 ContentAlignment contentAlignment = ContentAlignment.TopLeft;
-                Bitmap image = Image.Thumbnail(bmp, new Size(width, (int)height), contentAlignment, ThumbnailType.Photo);
-                Image.SaveIamge(image, 80L, newFileName);
+                Bitmap image = ImageHelper.Thumbnail(bmp, new Size(width, (int)height), contentAlignment, ThumbnailType.Photo);
+                ImageHelper.SaveIamge(image, 80L, newFileName);
                 return 1;
             }
             return 0;
@@ -99,171 +102,225 @@ namespace AICore.Utils
         public static void MakeThumbnailImage(string fileName, string newFileName, int width, int height, ThumbnailType type = ThumbnailType.Photo)
         {
             Bitmap bmp = new Bitmap(fileName);
-            Image.ThumbMode mode = mode = Image.ThumbMode.Max;
+            ImageHelper.ThumbMode mode = mode = ImageHelper.ThumbMode.Max;
             ContentAlignment contentAlignment = ContentAlignment.TopLeft;
-            Bitmap image = Image.Thumbnail(bmp, new Size(width, height), contentAlignment, type);
-            Image.SaveIamge(image, 100L, newFileName);
+            Bitmap image = ImageHelper.Thumbnail(bmp, new Size(width, height), contentAlignment, type);
+            ImageHelper.SaveIamge(image, 100L, newFileName);
         }
 
-        public class Image
+
+    }//end class
+
+    public class ImageHelper
+    {
+        /// <summary>
+        /// 缩略模式。
+        /// </summary>
+        public enum ThumbMode : byte
         {
             /// <summary>
-            /// 缩略模式。
+            /// 完整模式
             /// </summary>
-            public enum ThumbMode : byte
-            {
-                /// <summary>
-                /// 完整模式
-                /// </summary>
-                Full = 1,
-                /// <summary>
-                /// 最大尺寸
-                /// </summary>
-                Max
-            }
-
+            Full = 1,
             /// <summary>
-            /// 缩略图。
+            /// 最大尺寸
             /// </summary>
-            /// <param name="image">要缩略的图片</param>
-            /// <param name="size">要缩放的尺寸</param>
-            /// <param name="mode">缩略模式</param>
-            /// <param name="contentAlignment">对齐方式</param>
-            /// <returns>返回已经缩放的图片。</returns>
-            public static Bitmap Thumbnail(Bitmap image, Size size, ContentAlignment contentAlignment, ThumbnailType type)
+            Max
+        }
+
+        /// <summary>
+        /// 缩略图。
+        /// </summary>
+        /// <param name="image">要缩略的图片</param>
+        /// <param name="size">要缩放的尺寸</param>
+        /// <param name="mode">缩略模式</param>
+        /// <param name="contentAlignment">对齐方式</param>
+        /// <returns>返回已经缩放的图片。</returns>
+        public static Bitmap Thumbnail(Bitmap image, Size size, ContentAlignment contentAlignment, ThumbnailType type)
+        {
+
+            if (image.Width <= size.Width && image.Height <= size.Height)
             {
-
-                if (image.Width <= size.Width && image.Height <= size.Height)
-                {
-                    if (type == ThumbnailType.Photo)
-                        return image;
-                }
-
-                if (!size.IsEmpty && !image.Size.IsEmpty && !size.Equals(image.Size))
-                {
-                    //先取一个宽比例。
-                    double scale = (double)image.Width / (double)size.Width;
-
-                    if (image.Height > image.Width)
-                        scale = (double)image.Height / (double)size.Height;
-                    //缩略模式
-                    //switch (mode)
-                    //{
-                    //    case ThumbMode.Full:
-                    //        if (image.Height > image.Width)
-                    //            scale = (double)image.Height / (double)size.Height;
-                    //        break;
-                    //    case ThumbMode.Max:
-                    //        if (image.Height / scale < size.Height)
-                    //            scale = (double)image.Height / (double)size.Height;
-                    //        break;
-                    //}
-
-                    Size newSzie = new Size((int)(image.Width / scale), (int)(image.Height / scale));
-                    Bitmap result = new Bitmap(newSzie.Width, newSzie.Height);
-
-                    if (type == ThumbnailType.Logo)
-                    {
-                        newSzie = new Size(size.Width, size.Height);
-                        result = new Bitmap(newSzie.Width, newSzie.Height);
-                    }
-
-                    using (Graphics g = Graphics.FromImage(result))
-                    {
-                        //背景颜色
-                        g.FillRectangle(Brushes.Transparent, new Rectangle(new Point(0, 0), size));
-                        g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                        g.SmoothingMode = SmoothingMode.HighQuality;
-                        g.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                        g.CompositingMode = CompositingMode.SourceOver;
-                        g.CompositingQuality = CompositingQuality.HighQuality;
-                        g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
-                        //对齐方式
-                        Rectangle destRect;
-                        switch (contentAlignment)
-                        {
-                            case ContentAlignment.TopCenter:
-                                destRect = new Rectangle(new Point(-((newSzie.Width - size.Width) / 2), 0), newSzie);
-                                break;
-                            case ContentAlignment.TopRight:
-                                destRect = new Rectangle(new Point(-(newSzie.Width - size.Width), 0), newSzie);
-                                break;
-                            case ContentAlignment.MiddleLeft:
-                                destRect = new Rectangle(new Point(0, -((newSzie.Height - size.Height) / 2)), newSzie);
-                                break;
-                            case ContentAlignment.MiddleCenter:
-                                destRect = new Rectangle(new Point(-((newSzie.Width - size.Width) / 2), -((newSzie.Height - size.Height) / 2)), newSzie);
-                                break;
-                            case ContentAlignment.MiddleRight:
-                                destRect = new Rectangle(new Point(-(newSzie.Width - size.Width), -((newSzie.Height - size.Height) / 2)), newSzie);
-                                break;
-                            case ContentAlignment.BottomLeft:
-                                destRect = new Rectangle(new Point(0, -(newSzie.Height - size.Height)), newSzie);
-                                break;
-                            case ContentAlignment.BottomCenter:
-                                destRect = new Rectangle(new Point(-((newSzie.Width - size.Width) / 2), -(newSzie.Height - size.Height)), newSzie);
-                                break;
-                            case ContentAlignment.BottomRight:
-                                destRect = new Rectangle(new Point(-(newSzie.Width - size.Width), -(newSzie.Height - size.Height)), newSzie);
-                                break;
-                            default:
-                                destRect = new Rectangle(new Point(0, 0), newSzie);
-                                break;
-                        }
-                        g.DrawImage(image, destRect, new Rectangle(new Point(0, 0), image.Size), GraphicsUnit.Pixel);
-                        image.Dispose();
-                    }
-                    return result;
-                }
-                else
+                if (type == ThumbnailType.Photo)
                     return image;
             }
 
-            /// <summary>
-            /// 保存图片。
-            /// </summary>
-            /// <param name="image">要保存的图片</param>
-            /// <param name="quality">品质（1L~100L之间，数值越大品质越好）</param>
-            /// <param name="filename">保存路径</param>
-            public static void SaveIamge(Bitmap image, long quality, string filename)
+            if (!size.IsEmpty && !image.Size.IsEmpty && !size.Equals(image.Size))
             {
-                using (EncoderParameters encoderParams = new EncoderParameters(1))
-                {
-                    using (EncoderParameter parameter = (encoderParams.Param[0] = new EncoderParameter(Encoder.Quality, quality)))
-                    {
-                        ImageCodecInfo encoder = null;
-                        //取得扩展名
-                        string ext = Path.GetExtension(filename);
-                        if (string.IsNullOrEmpty(ext))
-                            ext = ".jpg";
-                        //根据扩展名得到解码、编码器
-                        foreach (ImageCodecInfo codecInfo in ImageCodecInfo.GetImageEncoders())
-                        {
-                            if (Regex.IsMatch(codecInfo.FilenameExtension, string.Format(@"(;|^)\*\{0}(;|$)", ext), RegexOptions.IgnoreCase))
-                            {
-                                encoder = codecInfo;
-                                break;
-                            }
-                        }
-                        Directory.CreateDirectory(Path.GetDirectoryName(filename));
-                        image.Save(filename, encoder, encoderParams);
-                    }
-                }
-            }
+                //先取一个宽比例。
+                double scale = (double)image.Width / (double)size.Width;
 
-            /// <summary>
-            /// 保存图片。
-            /// </summary>
-            /// <param name="stream">要保存的流</param>
-            /// <param name="quality">品质（1L~100L之间，数值越大品质越好）</param>
-            /// <param name="filename">保存路径</param>
-            public static void SaveIamge(Stream stream, long quality, string filename)
-            {
-                using (Bitmap bmpTemp = new Bitmap(stream))
+                if (image.Height > image.Width)
+                    scale = (double)image.Height / (double)size.Height;
+                //缩略模式
+                //switch (mode)
+                //{
+                //    case ThumbMode.Full:
+                //        if (image.Height > image.Width)
+                //            scale = (double)image.Height / (double)size.Height;
+                //        break;
+                //    case ThumbMode.Max:
+                //        if (image.Height / scale < size.Height)
+                //            scale = (double)image.Height / (double)size.Height;
+                //        break;
+                //}
+
+                Size newSzie = new Size((int)(image.Width / scale), (int)(image.Height / scale));
+                Bitmap result = new Bitmap(newSzie.Width, newSzie.Height);
+
+                if (type == ThumbnailType.Logo)
                 {
-                    SaveIamge(bmpTemp, quality, filename);
+                    newSzie = new Size(size.Width, size.Height);
+                    result = new Bitmap(newSzie.Width, newSzie.Height);
+                }
+
+                using (Graphics g = Graphics.FromImage(result))
+                {
+                    //背景颜色
+                    g.FillRectangle(Brushes.Transparent, new Rectangle(new Point(0, 0), size));
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    g.SmoothingMode = SmoothingMode.HighQuality;
+                    g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    g.CompositingMode = CompositingMode.SourceOver;
+                    g.CompositingQuality = CompositingQuality.HighQuality;
+                    g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                    //对齐方式
+                    Rectangle destRect;
+                    switch (contentAlignment)
+                    {
+                        case ContentAlignment.TopCenter:
+                            destRect = new Rectangle(new Point(-((newSzie.Width - size.Width) / 2), 0), newSzie);
+                            break;
+                        case ContentAlignment.TopRight:
+                            destRect = new Rectangle(new Point(-(newSzie.Width - size.Width), 0), newSzie);
+                            break;
+                        case ContentAlignment.MiddleLeft:
+                            destRect = new Rectangle(new Point(0, -((newSzie.Height - size.Height) / 2)), newSzie);
+                            break;
+                        case ContentAlignment.MiddleCenter:
+                            destRect = new Rectangle(new Point(-((newSzie.Width - size.Width) / 2), -((newSzie.Height - size.Height) / 2)), newSzie);
+                            break;
+                        case ContentAlignment.MiddleRight:
+                            destRect = new Rectangle(new Point(-(newSzie.Width - size.Width), -((newSzie.Height - size.Height) / 2)), newSzie);
+                            break;
+                        case ContentAlignment.BottomLeft:
+                            destRect = new Rectangle(new Point(0, -(newSzie.Height - size.Height)), newSzie);
+                            break;
+                        case ContentAlignment.BottomCenter:
+                            destRect = new Rectangle(new Point(-((newSzie.Width - size.Width) / 2), -(newSzie.Height - size.Height)), newSzie);
+                            break;
+                        case ContentAlignment.BottomRight:
+                            destRect = new Rectangle(new Point(-(newSzie.Width - size.Width), -(newSzie.Height - size.Height)), newSzie);
+                            break;
+                        default:
+                            destRect = new Rectangle(new Point(0, 0), newSzie);
+                            break;
+                    }
+                    g.DrawImage(image, destRect, new Rectangle(new Point(0, 0), image.Size), GraphicsUnit.Pixel);
+                    image.Dispose();
+                }
+                return result;
+            }
+            else
+                return image;
+        }
+
+        /// <summary>
+        /// 保存图片。
+        /// </summary>
+        /// <param name="image">要保存的图片</param>
+        /// <param name="quality">品质（1L~100L之间，数值越大品质越好）</param>
+        /// <param name="filename">保存路径</param>
+        public static void SaveIamge(Bitmap image, long quality, string filename)
+        {
+            using (EncoderParameters encoderParams = new EncoderParameters(1))
+            {
+                using (EncoderParameter parameter = (encoderParams.Param[0] = new EncoderParameter(Encoder.Quality, quality)))
+                {
+                    ImageCodecInfo encoder = null;
+                    //取得扩展名
+                    string ext = Path.GetExtension(filename);
+                    if (string.IsNullOrEmpty(ext))
+                        ext = ".jpg";
+                    //根据扩展名得到解码、编码器
+                    foreach (ImageCodecInfo codecInfo in ImageCodecInfo.GetImageEncoders())
+                    {
+                        if (Regex.IsMatch(codecInfo.FilenameExtension, string.Format(@"(;|^)\*\{0}(;|$)", ext), RegexOptions.IgnoreCase))
+                        {
+                            encoder = codecInfo;
+                            break;
+                        }
+                    }
+                    Directory.CreateDirectory(Path.GetDirectoryName(filename));
+                    image.Save(filename, encoder, encoderParams);
                 }
             }
         }
-    }//end class
+
+        /// <summary>
+        /// 保存图片。
+        /// </summary>
+        /// <param name="stream">要保存的流</param>
+        /// <param name="quality">品质（1L~100L之间，数值越大品质越好）</param>
+        /// <param name="filename">保存路径</param>
+        public static void SaveIamge(Stream stream, long quality, string filename)
+        {
+            using (Bitmap bmpTemp = new Bitmap(stream))
+            {
+                SaveIamge(bmpTemp, quality, filename);
+            }
+        }
+
+        /// <summary>
+        /// 合成图片
+        /// </summary>
+        /// <param name="file">要合成的图片信息</param>
+        /// <param name="fileList">合成的图片列表</param>
+        /// <param name="textList">合成的文字列表</param>
+        public static void MergeImage(FileMergeSize file, IList<FileMergeSize> fileList, IList<FontTextSize> textList)
+        {
+            Bitmap bit0 = new Bitmap(file.Width, file.Height);
+            // 初始化画板
+            Graphics g1 = Graphics.FromImage(bit0);
+            //设置画布背景颜色为白色
+            g1.FillRectangle(Brushes.White, new Rectangle(0, 0, file.Width, file.Height));
+            foreach (var item in fileList)
+            {
+                using (Bitmap bit1 = new Bitmap(item.FileName))
+                {
+                    g1.DrawImage(bit1, item.X, item.Y, bit1.Width, bit1.Height);
+                }
+            }
+            foreach (var item in textList)
+            {
+                g1.DrawString(item.Text, item.TextFont, item.TextBrush, item.X, item.Y);
+            }
+            bit0.Save(file.FileName);
+            bit0.Dispose();
+        }
+    }
+
+    public class FileMergeSize
+    {
+        public string FileName { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public class FontTextSize
+    {
+        public FontTextSize()
+        {
+            this.TextBrush = new SolidBrush(Color.Black);
+            this.TextFont = new Font("微软雅黑",24);
+
+        }
+        public string Text { get; set; }
+        public Brush TextBrush { get; set; }
+        public Font TextFont { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
 }
--- FMerge.Web/Controllers/HomeController.cs
+++ FMerge.Web/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using AICore.Utils;
 using Microsoft.AspNetCore.Http;
 using FMerge.Web.Component;
 using AICore.Domain.Model;
+using System.DrawingCore;
 
 namespace FMerge.Web.Controllers
 {
@@ -25,6 +26,53 @@ namespace FMerge.Web.Controllers
             // var dt = us.getModel(10);
             // ViewBag.kk2 = dt.Id;
             return View();
+        }
+
+        public IActionResult T()
+        {
+            // FileMergeSize fs = new FileMergeSize();
+            // fs.Width = 579;
+            // fs.Height = 680;
+            // fs.FileName = "e:\\1.jpg";
+            // List<FileMergeSize> fslist = new List<FileMergeSize>();
+            // FileMergeSize fs1 = new FileMergeSize();
+            // fs1.FileName = "e:\\0.jpg";
+            // FileMergeSize fs2 = new FileMergeSize();
+            // fs2.FileName = "e:\\qr.png";
+            // fs2.X = 420;
+            // fs2.Y = 540;
+            // fslist.Add(fs1);
+            // fslist.Add(fs2);
+            // List<FontTextSize> ftlist = new List<FontTextSize>();
+            // FontTextSize ft1 = new FontTextSize();
+            // ft1.Text = "我是"; 
+            // ft1.Y = 540;
+            // FontTextSize ft2 = new FontTextSize();
+            // ft2.Text = "大学生";
+            // ft2.TextBrush = new SolidBrush(Color.Red);
+            // ft2.X = 64;
+            // ft2.Y = 540;
+            // FontTextSize ft3 = new FontTextSize();
+            // ft3.Text = "2018年";
+            // ft3.Y = 580;
+            // ft3.TextBrush = new SolidBrush(Color.Red);
+            // FontTextSize ft4 = new FontTextSize();
+            // ft4.Text = "毕业于";
+            // //ft4.X = ft3.Text.Length * 30;
+            // ft4.Y = 620;
+            // FontTextSize ft5 = new FontTextSize();
+            // ft5.Text = "重庆大学";
+            // ft5.X = 96;
+            // ft5.Y = 620;
+            // ft5.TextBrush = new SolidBrush(Color.Red);
+
+            // ftlist.Add(ft1);
+            // ftlist.Add(ft2);
+            // ftlist.Add(ft3);
+            // ftlist.Add(ft4);
+            // ftlist.Add(ft5);
+            // ImageHelper.MergeImage(fs, fslist, ftlist);
+            return Content("yes");
         }
 
         
